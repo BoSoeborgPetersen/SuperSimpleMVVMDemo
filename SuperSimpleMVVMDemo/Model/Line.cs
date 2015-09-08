42fa76271c8f04548a0f4e22c7265b4ef236c027
Bo Petersen <bspet@elektro.dtu.dk>
2015-09-08 16:42:21 +0200

    Initial Real Version

---
+++ SuperSimpleMVVMDemo/Model/Line.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperSimpleMVVMDemo.Model
+{
+    // The Line class has a reference to 2 shapes, that it connects.
+    public class Line
+    {
+        // Regions can be used to make code foldable (minus/plus sign to the left).
+        #region Properties
+
+        // The "{ get; set; }" syntax describes that a private field 
+        //  and default getter setter methods should be generated.
+        //  This is called Auto-Implemented Properties (http://msdn.microsoft.com/en-us/library/bb384054.aspx).
+        // Java:
+        //  private Shape from;
+        // 
+        //  public Shape getFrom(){
+        //    return from;
+        //  }
+        //
+        //  public void setFrom(Shape value){
+        //    from = value;
+        //  }
+
+        public Shape From { get; set; }
+
+        // Java:
+        //  private Shape to;
+        //  
+        //  public Shape getTo(){
+        //    return to;
+        //  }
+        //
+        //  public void setTo(Shape value){
+        //    to = value;
+        //  }
+        public Shape To { get; set; }
+
+        #endregion
+    }
+}
---
+++ SuperSimpleMVVMDemo/Model/Shape.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperSimpleMVVMDemo.Model
+{
+    // The Shape class descripes a shape with a position (X and Y), and a size (Width and Height).
+    public class Shape
+    {
+        // Regions can be used to make code foldable (minus/plus sign to the left).
+        #region Properties
+
+        // The "{ get; set; }" syntax describes that a private field 
+        //  and default getter setter methods should be generated.
+        //  This is called Auto-Implemented Properties (http://msdn.microsoft.com/en-us/library/bb384054.aspx).
+
+        // Java:
+        //  private int x = 200;
+        // 
+        //  public int getX(){
+        //    return x;
+        //  }
+        //
+        //  public void setX(int value){
+        //    x = value;
+        //  }
+        public int X { get; set; } = 200;
+
+        // Java:
+        //  private int y = 200;
+        // 
+        //  public int getY(){
+        //    return y;
+        //  }
+        //
+        //  public void setY(int value){
+        //    y = value;
+        //  }
+        public int Y { get; set; } = 200;
+
+        // Java:
+        //  private int width = 100;
+        // 
+        //  public int getWidth(){
+        //    return width;
+        //  }
+        //
+        //  public void setWidth(int value){
+        //    width = value;
+        //  }
+        public int Width { get; set; } = 100;
+
+        // Java:
+        //  private int height = 100;
+        // 
+        //  public int getHeight(){
+        //    return height;
+        //  }
+        //
+        //  public void setHeight(int value){
+        //    height = value;
+        //  }
+        public int Height { get; set; } = 100;
+
+        #endregion
+
+        #region Derived Properties
+
+        // The CenterX and CenterY derived properties are used by the Line class, 
+        //  so it can be drawn from the center of one Shape to the center of another Shape.
+        // These methods use an expression-bodied member (http://www.informit.com/articles/article.aspx?p=2414582) to simplify methods that only returns a value;
+
+        // Java:
+        //  public int getCenterX(){
+        //    return X + Width / 2;
+        //  }
+        // Old .NET (Before 4.6):
+        //  public int CenterX { get { return X + Width / 2; } }
+        public int CenterX => X + Width / 2;
+
+        // Java:
+        //  public int getCenterY(){
+        //    return Y + Height / 2;
+        //  }
+        // Old .NET (Before 4.6):
+        //  public int CenterY { get { return Y + Height / 2; } }
+        public int CenterY => Y + Height / 2;
+
+        #endregion
+    }
+}
